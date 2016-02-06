dd3e6a5e32f8e5ad95f8864547d3a777341e56a6
Łukasz Lalik <lukasz.lalik@gmail.com>
2016-02-06 19:52:04 +0100

    Parsing arguments with day and part to run

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,7 +1,30 @@
 (ns advent-of-code.core
   (:gen-class))
 
+
+(defn- call-mapping-function
+    [mapping day part]
+    (if (contains? mapping day)
+        (let [day-mapping (mapping day)]
+            (if (contains? day-mapping part)
+                (let [part-fn (day-mapping part)]
+                    (part-fn))
+                (str part " for " day " is not implemented")))
+        (str day " is not implemented")))
+
+(defn call-day-and-part
+    "Calls appropriate function for day and part given as a parameter"
+    [mapping & args]
+    (if (>= (count args) 2)
+        (let [day (nth args 0)
+              part (nth args 1)]
+              (call-mapping-function mapping day part))
+        "Please provide day and part number. E.g. advent-of-code day5 part2"
+        ))
+
+(def implemented {})
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (call-day-and-part args)))
--- test/advent_of_code/core_test.clj
+++ test/advent_of_code/core_test.clj
@@ -1,7 +1,32 @@
 (ns advent-of-code.core-test
   (:require [clojure.test :refer :all]
-            [advent-of-code.core :refer :all]))
+            [advent-of-code.core :refer :all]
+            [clojure.string :as string]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def mapping {
+    "day1" {
+        "part1" (fn [] "day1 part1 called")
+    }})
+
+(deftest call-day-and-part-test
+    (testing "Call day and part from arguments"
+        (testing "with no arguments provided"
+            (is (string/includes?
+                    (call-day-and-part mapping)
+                    "Please provide day and part number")))
+        (testing "with only day provided"
+            (is (string/includes?
+                    (call-day-and-part mapping "day1")
+                    "Please provide day and part number")))
+        (testing "with valid day and part provided"
+            (is (string/includes?
+                    (call-day-and-part mapping "day1" "part1")
+                    "day1 part1 called")))
+        (testing "with invalid day provided"
+            (is (string/includes?
+                    (call-day-and-part mapping "day5" "part1")
+                    "day5 is not implemented")))
+        (testing "with valid day but invalid part"
+            (is (string/includes?
+                    (call-day-and-part mapping "day1" "part2")
+                    "part2 for day1 is not implemented")))))
